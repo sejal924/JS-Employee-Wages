ece2b24236309cf8e049e84e013f0517b9d568cd
sejal924 <sejalchouhan24@gmail.com>
2025-03-06 11:08:46 +0530

    [Sejal]feat.Extending employee payroll data to store gender and start date

--- Employee_Wages/EmployeePayrollData.js
+++ Employee_Wages/EmployeePayrollData.js
@@ -6,12 +6,16 @@ class EmployeePayrollData {
   id;
   salary;
   name;
+  gender;
+  startDate;
 
   //constructor
-  constructor(id, name, salary) {
+  constructor(id, name, salary, gender, startDate) {
     this.id = id;
     this.salary = salary;
     this.name = name;
+    this.gender = gender;
+    this.startDate = startDate;
   }
 
   //getter and setter method
@@ -26,7 +30,16 @@ class EmployeePayrollData {
   //method
   toString() {
     return (
-      "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary
+      "id = " +
+      this.id +
+      ", name = " +
+      this.name +
+      ", salary = " +
+      this.salary +
+      ", gender = " +
+      this.gender +
+      ", startDate = " +
+      this.startDate
     );
   }
 }
@@ -35,3 +48,11 @@ let employeePayrollData = new EmployeePayrollData(1, "Aryan", 50000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "Arjun";
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(
+  1,
+  "Arshi",
+  30000,
+  "F",
+  new Date()
+);
+console.log(newEmployeePayrollData.toString());
